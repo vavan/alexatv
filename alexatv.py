ecedaade3e64bb6e52aa9bf1f731a2e7fed750ec
Vova Tymoshchuk <volodymyr.tymoshchuk@gmail.com>
2018-02-06 05:23:45 +0000

    Extract Remote class, add comments
    - Exrtact all remote functions to separate class
    - Add comments to main functions
    - Pass logger to all

--- alexatv.py
+++ alexatv.py
@@ -18,6 +18,7 @@ def read_config():
     return config
 
 def init_logger():
+    '''Init MQTT logger, use it for all logging'''
     logger = logging.getLogger("AWSIoTPythonSDK.core")
     logger.handlers = []
     logger.setLevel(logging.INFO)
@@ -28,15 +29,21 @@ def init_logger():
     return logger
 
 class PowerSensor:
+    '''
+    Sensor for TV Power LED. If LED is on, then TV is on.
+    Use RC circuit approach to measure light sensor resistance.
+    When PowerSensor.enable is False the feature is off.
+    '''
     PIN = 23
     TIMEOUT = 50000
     THREASHOLD = 20000
-    enable=False
+    enable = False
     @staticmethod
-    def init(enable = False):
+    def init(logger, enable):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         PowerSensor.enable = enable
+        logger.info('Power sensor enable: %s'%enable)
     def read(self):
         value = 0
         GPIO.setup(self.PIN, GPIO.OUT)
@@ -50,72 +57,91 @@ class PowerSensor:
         value = self.read()
         return value < self.THREASHOLD
 
+class Remote:
+    '''
+    Class to implement remote IR key events.
+    Change it for your remote behavior.
+        * set_power: turn TV on and off, skip if the state is already done
+        * set_input: change the TV input, possible inputs are: xbox, roku/cable/netflix/movies and anythig else
+        * set_volume: volume up or down requested times
+        * set_mute: mute or unmute (this TV requires double click to mute)
+    '''
+    logger = None
+    @staticmethod
+    def init(logger):
+        Remote.logger = logger
+    @staticmethod
+    def set_power(arg):
+        if arg:
+            if PowerSensor.enable and PowerSensor().is_on():
+                Remote.logger.info('already on')
+            else:
+                Remote.logger.info('power on')
+                os.system('irsend SEND_ONCE CT-90325 KEY_POWER')
+        else:
+            if PowerSensor.enable and not PowerSensor().is_on():
+                Remote.logger.info('already off')
+            else:
+                Remote.logger.info('power off')
+                os.system('irsend SEND_ONCE CT-90325 KEY_POWER')
+    @staticmethod
+    def set_input(arg):
+        if arg == 'xbox':
+            Remote.logger.info('xbox')
+            os.system('irsend SEND_ONCE CT-90325 KEY_CYCLEWINDOWS')
+            os.system('irsend SEND_ONCE CT-90325 KEY_3')
+        elif arg in ('roku', 'cable', 'netflix', 'movies'):
+            Remote.logger.info('roku')
+            os.system('irsend SEND_ONCE CT-90325 KEY_CYCLEWINDOWS')
+            os.system('irsend SEND_ONCE CT-90325 KEY_2')
+        else:
+            Remote.logger.info('hdmi3')
+            os.system('irsend SEND_ONCE CT-90325 KEY_CYCLEWINDOWS')
+            os.system('irsend SEND_ONCE CT-90325 KEY_4')
+    @staticmethod
+    def set_volume(arg):
+        if arg > 0:
+            Remote.logger.info('volume up %s'%arg)
+            for i in range(arg):
+                os.system('irsend SEND_ONCE CT-90325 KEY_VOLUMEUP')
+        else:
+            Remote.logger.info('volume do %s'%arg)
+            for i in range(-arg):
+                os.system('irsend SEND_ONCE CT-90325 KEY_VOLUMEDOWN')
+    @staticmethod
+    def set_mute(arg):
+        if arg == 'True':
+            Remote.logger.info('mute')
+            os.system('irsend SEND_ONCE CT-90325 KEY_MUTE')
+            os.system('irsend SEND_ONCE CT-90325 KEY_MUTE')
+        else:
+            Remote.logger.info('unmute')
+            os.system('irsend SEND_ONCE CT-90325 KEY_VOLUMEUP')
+
 def mqtt_callback(client, userdata, message):
     logger = logging.getLogger("AWSIoTPythonSDK.core")
     cmd, arg = message.payload.split(':')
     if cmd == 'power':
-        if arg == 'ON':
-            if PowerSensor.enable and PowerSensor().is_on():
-                logger.info('already on')
-            else:
-                logger.info('power on')
-                os.system('irsend SEND_ONCE CT-90325 KEY_POWER')
-        else:
-            if PowerSensor.enable and not PowerSensor().is_on():
-                logger.info('already off')
-            else:
-                logger.info('power off')
-                os.system('irsend SEND_ONCE CT-90325 KEY_POWER')
+        Remote.set_power(arg == 'ON')
     elif cmd == 'input':
-        arg = arg.lower()
-        if arg == 'xbox':
-            logger.info('xbox')
-            os.system('irsend SEND_ONCE CT-90325 KEY_CYCLEWINDOWS')
-            os.system('irsend SEND_ONCE CT-90325 KEY_3')
-        elif arg in ('roku', 'cable', 'netflix', 'movies'):
-            logger.info('roku')
-            os.system('irsend SEND_ONCE CT-90325 KEY_CYCLEWINDOWS')
-            os.system('irsend SEND_ONCE CT-90325 KEY_2')
-        else:
-            logger.info('hdmi3')
-            os.system('irsend SEND_ONCE CT-90325 KEY_CYCLEWINDOWS')
-            os.system('irsend SEND_ONCE CT-90325 KEY_4')
+        Remote.set_input(arg.lower())
     elif cmd == 'volume':
-        arg = int(arg)
-        if arg > 0:
-            logger.info('volume up %s'%arg)
-            for i in range(arg):
-                os.system('irsend SEND_ONCE CT-90325 KEY_VOLUMEUP')
-        else:
-            logger.info('volume do %s'%arg)
-            for i in range(-arg):
-                os.system('irsend SEND_ONCE CT-90325 KEY_VOLUMEDOWN')
+        Remote.set_volume(int(arg))
     elif cmd == 'mute':
-        if arg == 'True':
-            logger.info('mute')
-            os.system('irsend SEND_ONCE CT-90325 KEY_MUTE')
-            os.system('irsend SEND_ONCE CT-90325 KEY_MUTE')
-        else:
-            logger.info('unmute')
-            os.system('irsend SEND_ONCE CT-90325 KEY_VOLUMEUP')
+        Remote.set_mute(arg)
 
-
-
-def init_mqtt(config, logger):
+def init_mqtt(logger, config):
     clientId = 'basicPubSub'
-    iot_config = dict(config.items('aws_iot'))
-
     # Init AWSIoTMQTTClient
     myAWSIoTMQTTClient = None
     myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
-    myAWSIoTMQTTClient.configureEndpoint(iot_config['endpoint'], 8883)
-    myAWSIoTMQTTClient.configureCredentials(iot_config['root_ca'], iot_config['private'], iot_config['certificate'])
+    myAWSIoTMQTTClient.configureEndpoint(config['endpoint'], 8883)
+    myAWSIoTMQTTClient.configureCredentials(config['root_ca'], config['private'], config['certificate'])
     myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
     myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
     myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
     myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
     myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
-
     for i in range(5):
         try:
             # Connect and subscribe to AWS IoT
@@ -123,17 +149,15 @@ def init_mqtt(config, logger):
             myAWSIoTMQTTClient.subscribe(MQTT_TOPIC, 1, mqtt_callback)
             break
         except Exception:
-            logging.exception("We got a MQTT trouble")
+            logging.exception("We got a MQTT problem, let's try again")
             time.sleep(3)
-
 
 if __name__ == '__main__':
     config = read_config()
     logger = init_logger()
-    sensor_enabled = config.getboolean('remote', 'sensor_enabled')
-    logger.info('Power sensor enable: %s'%sensor_enabled)
-    PowerSensor.init(enable = sensor_enabled)
-    init_mqtt(config, logger)
+    PowerSensor.init(logger, config.getboolean('remote', 'sensor_enabled'))
+    Remote.init(logger)
+    init_mqtt(logger, dict(config.items('aws_iot')))
     while True:
         time.sleep(10)
 
